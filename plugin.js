130050ce37bd369eb5a563956e404787088cafd5
KickVanelli <55439825+KickVanelli@users.noreply.github.com>
2019-09-17 15:54:34 +0200

    Init
    Initieel

--- plugin.js
+++ plugin.js
@@ -17,9 +17,9 @@ define(['playbackManager', 'skinManager', 'userSettings', 'pluginManager', 'brow
 
         var self = this;
 
-        self.name = 'Default Skin';
+        self.name = 'Kick Skin';
         self.type = 'skin';
-        self.id = 'defaultskin';
+        self.id = 'kickskin';
 
         var clockInterval;
         self.load = function () {
